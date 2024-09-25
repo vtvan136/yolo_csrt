c4e6cd21e1bb836434a74d7f446faf97343965a3
vtvan <vtvan136@gmail.com>
2024-09-25 16:01:24 +0700

    update-generation

--- app/models/generate_frames.py
+++ app/models/generate_frames.py
@@ -4,90 +4,16 @@ from ultralytics import YOLO
 
 model = YOLO("E:/HW/app/models/yolov8n.pt")
 
-frame_count = 0
 
-tracker = None
-steps_detected = []  # Stores detected steps with timestamps
-steps_executed = []  # Steps that have been executed
-last_step = None
-step_frequency = {}
-tracking_start_time = None
-detect_interval = 25
-execution_threshold = 20
-confidence_threshold = 0.7
-def get_color_for_step(step):
-    # Define colors for different steps; add more as needed
-    colors = [
-        (0, 255, 0),   # Green
-        (0, 0, 255),   # Red
-        (255, 0, 0),   # Blue
-        (255, 255, 0), # Cyan
-        (0, 255, 255), # Yellow
-        (255, 0, 255), # Magenta
-    ]
-    return colors[step % len(colors)]
-
-def detect_and_track(frame, frame_time,socketio):
-    global tracker, last_step, steps_detected, tracking_start_time
-
-    results = model(frame)
-    detections = results[0].boxes.data.cpu().numpy()
-
-    if len(detections) > 0:
-        for det in detections:
-            if len(det) >= 6:
-                x1, y1, x2, y2, conf, cls = det[:6]
-            elif len(det) >= 4:
-                x1, y1, x2, y2 = det[:4]
-                conf = 1.0
-                cls = 0
-            else:
-                continue
-
-            if conf > confidence_threshold:
-                current_step = int(cls)
-
-                if tracking_start_time is None:
-                    tracking_start_time = frame_time
-
-                if current_step != last_step and current_step not in [s[0] for s in steps_detected]:
-                    tracker = cv2.TrackerCSRT_create()
-                    # Ensure the bounding box is within frame boundaries
-                    height, width, _ = frame.shape
-                    x1 = max(0, min(int(x1), width - 1))
-                    y1 = max(0, min(int(y1), height - 1))
-                    x2 = max(0, min(int(x2), width - 1))
-                    y2 = max(0, min(int(y2), height - 1))
-
-                    bbox = (x1, y1, x2 - x1, y2 - y1)
-                    tracker.init(frame, bbox)
-                    last_step = current_step
-                    steps_detected.append((current_step, frame_time))
-                    socketio.emit('update_list_steps', {'list_steps': steps_detected})
-                    break
-
-def update_tracker(frame):
-    global tracker, last_step, step_frequency, steps_executed
-
-    if tracker is not None:
-        success, bbox = tracker.update(frame)
-        if success:
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            step_color = get_color_for_step(last_step)
-            cv2.rectangle(frame, p1, p2, step_color, 2, 1)
-
-            # Draw the step number on the box
-            step_text = f'Step: {last_step + 1 }'  # Text to display
-            cv2.putText(frame, step_text, (p1[0] + 10, p1[1] - 10 if p1[1] - 10 > 10 else p1[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, step_color, 2)
-
-            if last_step not in step_frequency:
-                step_frequency[last_step] = 0
-            step_frequency[last_step] += 1
-
-            if step_frequency[last_step] >= execution_threshold and last_step not in steps_executed:
-                steps_executed.append(last_step)
-                step_frequency[last_step] = 0
+def frame_to_image(frame):
+    # Mã hóa frame thành định dạng JPEG
+    success, encoded_image = cv2.imencode('.jpg', frame)
+    if success:
+        # Chuyển đổi ảnh đã mã hóa thành bytes
+        return encoded_image.tobytes()
+    else:
+        print("Error: Cannot encode frame.")
+        return None
 
 def middle_frame_video(video_path,output_image_path):
     cap = cv2.VideoCapture(video_path)
@@ -100,70 +26,161 @@ def middle_frame_video(video_path,output_image_path):
         if ret:
             cv2.imwrite(output_image_path, frame)
     cap.release()
-def frame_to_image(frame):
-    # Mã hóa frame thành định dạng JPEG
-    success, encoded_image = cv2.imencode('.jpg', frame)
-    if success:
-        # Chuyển đổi ảnh đã mã hóa thành bytes
-        return encoded_image.tobytes()
-    else:
-        print("Error: Cannot encode frame.")
-        return None
 
-def convert_to_seconds(steps_detected):
-    steps_in_seconds = [(step, format_time(time/ 1000) ) for step, time in steps_detected]
-    return steps_in_seconds
-def format_time(seconds):
-    total_seconds = math.floor(seconds)
-    minutes = total_seconds // 60
-    seconds = total_seconds % 60
-    return f"{minutes}:{seconds:02d}"
 
+object_custom_frequencies = {
+    0: 7,
+    1: 4,
+    2: 4,
+    3: 4,
+    4: 4,
+    5: 5
+}
 def generate_frames(cap,socketio):
-    global frame_count,frame_count,tracker ,steps_detected ,steps_executed,last_step,step_frequency,tracking_start_time
+    colors = [
+        (255, 0, 0),  # Màu đỏ
+        (0, 255, 0),  # Màu xanh lá
+        (0, 0, 255),  # Màu xanh dương
+        (255, 255, 0),  # Màu cyan
+        (255, 0, 255),  # Màu magenta
+        (0, 255, 255)  # Màu vàng
+    ]
+    # Khởi tạo CSRT tracker
+    tracker = cv2.TrackerCSRT_create()
+    tracking = False
+    last_object_id = None  # ID của đối tượng trước đó
+    frame_count = 0  # Biến đếm frame
+    no_detection_count = 0  # Biến đếm số frame không phát hiện
+    detected_objects = {}  # Lưu trữ các đối tượng và thời gian phát hiện
+    object_frequencies = {}  # Lưu tần suất xuất hiện của các đối tượng
+    object_color_map = {}
+    executed_objects = set()
+    object_detection_start_times = {}  # Thêm biến để lưu thời gian bắt đầu detect của đối tượng
     output_path = 'data/ouput_video_vp.webm'
     fourcc = cv2.VideoWriter_fourcc(*'vp80')  # Codec mp4
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-    while cap.isOpened():
+    frame_duration = 1 / fps
+    while True:
         ret, frame = cap.read()
         if not ret:
             break
-        frame_count += 1
-        frame_time = cap.get(cv2.CAP_PROP_POS_MSEC)
 
-        if (frame_count % detect_interval == 0 or tracker is None) and (
-                tracking_start_time is None or frame_time >= tracking_start_time):
-            detect_and_track(frame, frame_time,socketio)
+        frame_count += 1
+        current_video_time = frame_count * frame_duration  # Tính thời gian thực trong video
+
+        if not tracking or frame_count % 25 == 0:
+            results = model(frame)
+
+            if results:
+                detections = results[0]
+
+                if detections.boxes is not None and len(detections.boxes.xyxy) > 0:
+                    high_conf_indices = (detections.boxes.conf > 0.7).nonzero(as_tuple=True)[0]
+                    high_conf_boxes = detections.boxes.xyxy[high_conf_indices]
+                    high_conf_classes = detections.boxes.cls[high_conf_indices]
+
+                    if high_conf_boxes.size(0) > 0:
+                        no_detection_count = 0
+
+                        for i in range(high_conf_boxes.size(0)):
+                            current_bbox = high_conf_boxes[i].cpu().numpy()
+                            current_bbox = (
+                                int(current_bbox[0]), int(current_bbox[1]), int(current_bbox[2] - current_bbox[0]),
+                                int(current_bbox[3] - current_bbox[1]))
+                            current_object_id = int(high_conf_classes[i].item())
+
+                            # Bỏ qua nếu đối tượng đã có trong danh sách thực hiện
+                            if current_object_id in executed_objects:
+                                continue
+
+                            # Gán màu cho đối tượng nếu chưa có
+                            if current_object_id not in object_color_map:
+                                object_color_map[current_object_id] = colors[len(object_color_map) % len(colors)]
+
+                            # Lưu thời gian phát hiện
+                            if current_object_id not in detected_objects:
+                                detected_objects[
+                                    current_object_id] = current_video_time  # Lưu thời gian phát hiện theo thời gian video
+                                object_detection_start_times[
+                                    current_object_id] = current_video_time  # Lưu thời gian bắt đầu detect
+                                object_frequencies[current_object_id] = 1  # Khởi tạo tần suất
+
+
+                            else:
+                                if current_video_time - detected_objects[current_object_id] <= \
+                                        object_custom_frequencies[current_object_id]:
+                                    object_frequencies[current_object_id] += 1
+                                else:
+                                    detected_objects[current_object_id] = current_video_time
+                                    object_detection_start_times[
+                                        current_object_id] = current_video_time  # Cập nhật thời gian bắt đầu detect
+                                    object_frequencies[current_object_id] = 1
+                                    socketio.emit('update_list_steps', {'steps': object_detection_start_times})
+                            # Ghi lại các object có tần suất xuất hiện cao nhất trong 5 giây
+                            for obj_id, freq in object_frequencies.items():
+                                if freq > 2:
+                                    executed_objects.add(obj_id)
+
+                            # Khởi tạo tracker với bbox
+                            if not tracking or current_object_id != last_object_id:
+                                tracker.init(frame, current_bbox)
+                                tracking = True
+                                last_object_id = current_object_id
+                                break
+                    else:
+                        no_detection_count += 1
+
+                        if no_detection_count >= 2:
+                            tracking = False
+                            last_object_id = None
+
+
+                else:
+                    no_detection_count += 1
+
+                    if no_detection_count >= 2:
+                        tracking = False
+                        last_object_id = None
+
         else:
-            update_tracker(frame)
+            success, bbox = tracker.update(frame)
+            if success:
+                (x, y, w, h) = [int(v) for v in bbox]
+                x = max(0, x)
+                y = max(0, y)
+                w = min(w, frame.shape[1] - x)
+                h = min(h, frame.shape[0] - y)
+
+                object_color = object_color_map.get(last_object_id, (255, 255, 255))
+
+                cv2.rectangle(frame, (x, y), (x + w, y + h), object_color, 2)
+                label = f'Step : {last_object_id + 1}'
+                cv2.putText(frame, label, (x + 10, y - 10 if y - 10 > 10 else y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            object_color, 2)
         _, buffer = cv2.imencode('.jpg', frame)
         frame_bytes = buffer.tobytes()
-
-        # Gửi frame đến client qua WebSocket
         socketio.emit('video_frame', frame_bytes)
         socketio.sleep(1 / fps)
-        #cv2.imshow('Hand Washing Detection', frame)
+        cv2.imshow('Object Tracking', frame)
         out.write(frame)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
-    # Reset
-    frame_count = 0
-    tracker = None
-    steps_detected = []  # Stores detected steps with timestamps
-    steps_executed = []  # Steps that have been executed
-    last_step = None
-    step_frequency = {}
-    tracking_start_time = None
-    #cv2.destroyAllWindows()
-    steps_in_seconds = convert_to_seconds(steps_detected)
-    return steps_in_seconds, output_path
+    cv2.destroyAllWindows()
+    time_step = convert_dict_to_min_sec(object_detection_start_times)
+    return time_step,output_path
 
 
-
-
+def convert_dict_to_min_sec(input_dict):
+    result = []
+    for index, value in input_dict.items():
+        seconds = value
+        minutes = int(seconds // 60)
+        seconds = int(seconds % 60)
+        result.append((index, f"{minutes}:{seconds:02d}"))
+    return result
 
--- test/test_25_9.py
+++ test/test_25_9.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 from ultralytics import YOLO
 
@@ -5,88 +6,168 @@ from ultralytics import YOLO
 model = YOLO('E:\HW/app\models\yolov8n.pt')  # Bạn có thể thay đổi thành yolov8s.pt, yolov8m.pt, ...
 
 # Khởi tạo video
-video_source = 'E:\HW\data/test_video_TOD.mp4'  # Thay thế bằng đường dẫn video của bạn
-cap = cv2.VideoCapture(video_source)
+'''video_source = 'E:\HW\data/test_video_TOD.mp4'  # Thay thế bằng đường dẫn video của bạn
+cap = cv2.VideoCapture(video_source)'''
 
-# Khởi tạo CSRT tracker
-tracker = cv2.TrackerCSRT_create()
-tracking = False
-bbox = None
-last_object_id = None  # ID của đối tượng trước đó
 
-# Biến đếm frame
-frame_count = 0
-no_detection_count = 0  # Biến đếm số frame không phát hiện
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+object_custom_frequencies = {
+    0: 7,
+    1: 4,
+    2: 4,
+    3: 4,
+    4: 4,
+    5: 5
+}
+def generate_frame(cap,socketio):
+    colors = [
+        (255, 0, 0),  # Màu đỏ
+        (0, 255, 0),  # Màu xanh lá
+        (0, 0, 255),  # Màu xanh dương
+        (255, 255, 0),  # Màu cyan
+        (255, 0, 255),  # Màu magenta
+        (0, 255, 255)  # Màu vàng
+    ]
+    # Khởi tạo CSRT tracker
+    tracker = cv2.TrackerCSRT_create()
+    tracking = False
+    bbox = None
+    last_object_id = None  # ID của đối tượng trước đó
+    frame_count = 0  # Biến đếm frame
+    no_detection_count = 0  # Biến đếm số frame không phát hiện
+    detected_objects = {}  # Lưu trữ các đối tượng và thời gian phát hiện
+    object_frequencies = {}  # Lưu tần suất xuất hiện của các đối tượng
+    object_color_map = {}
+    executed_objects = set()
+    object_detection_start_times = {}  # Thêm biến để lưu thời gian bắt đầu detect của đối tượng
+    status_end = True
+    output_path = 'data/ouput_video_vp.webm'
+    fourcc = cv2.VideoWriter_fourcc(*'vp80')  # Codec mp4
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+    frame_duration = 1 / fps
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    frame_count += 1  # Tăng biến đếm frame
+        frame_count += 1
+        current_video_time = frame_count * frame_duration  # Tính thời gian thực trong video
 
-    # Nếu chưa theo dõi đối tượng hoặc đến thời điểm phát hiện lại
-    if not tracking or frame_count % 25 == 0:
-        # Phát hiện đối tượng
-        results = model(frame)
+        if not tracking or frame_count % 25 == 0:
+            results = model(frame)
 
-        # Kiểm tra xem có phát hiện đối tượng không
-        if results.xyxy[0].size(0) > 0:
-            # Lọc các bbox có confidence > 0.7
-            high_conf_boxes = results.xyxy[0][results.xyxy[0][:, 4] > 0.7]  # Lọc theo confidence
+            if results:
+                detections = results[0]
 
-            if high_conf_boxes.size(0) > 0:
-                # Reset biến đếm không phát hiện
-                no_detection_count = 0
+                if detections.boxes is not None and len(detections.boxes.xyxy) > 0:
+                    high_conf_indices = (detections.boxes.conf > 0.7).nonzero(as_tuple=True)[0]
+                    high_conf_boxes = detections.boxes.xyxy[high_conf_indices]
+                    high_conf_classes = detections.boxes.cls[high_conf_indices]
 
-                # Lấy bbox đầu tiên có confidence cao
-                current_bbox = high_conf_boxes[0][:4].cpu().numpy()  # bbox: [x1, y1, x2, y2]
-                current_bbox = (int(current_bbox[0]), int(current_bbox[1]), int(current_bbox[2] - current_bbox[0]), int(current_bbox[3] - current_bbox[1]))
+                    if high_conf_boxes.size(0) > 0:
+                        no_detection_count = 0
 
-                # Kiểm tra xem đối tượng có khác với đối tượng trước đó không
-                current_object_id = int(high_conf_boxes[0][5])  # ID của đối tượng (có thể là lớp hoặc ID cụ thể)
+                        for i in range(high_conf_boxes.size(0)):
+                            current_bbox = high_conf_boxes[i].cpu().numpy()
+                            current_bbox = (
+                                int(current_bbox[0]), int(current_bbox[1]), int(current_bbox[2] - current_bbox[0]),
+                                int(current_bbox[3] - current_bbox[1]))
+                            current_object_id = int(high_conf_classes[i].item())
 
-                if not tracking or current_object_id != last_object_id:
-                    # Khởi tạo tracker với bbox
-                    tracker.init(frame, current_bbox)
-                    tracking = True
-                    last_object_id = current_object_id  # Cập nhật ID đối tượng trước đó
+                            # Bỏ qua nếu đối tượng đã có trong danh sách thực hiện
+                            if current_object_id in executed_objects:
+                                continue
+
+                            # Gán màu cho đối tượng nếu chưa có
+                            if current_object_id not in object_color_map:
+                                object_color_map[current_object_id] = colors[len(object_color_map) % len(colors)]
+
+                            # Lưu thời gian phát hiện
+                            if current_object_id not in detected_objects:
+                                detected_objects[
+                                    current_object_id] = current_video_time  # Lưu thời gian phát hiện theo thời gian video
+                                object_detection_start_times[
+                                    current_object_id] = current_video_time  # Lưu thời gian bắt đầu detect
+                                object_frequencies[current_object_id] = 1  # Khởi tạo tần suất
+
+
+                            else:
+                                if current_video_time - detected_objects[current_object_id] <= \
+                                        object_custom_frequencies[current_object_id]:
+                                    object_frequencies[current_object_id] += 1
+                                else:
+                                    detected_objects[current_object_id] = current_video_time
+                                    object_detection_start_times[
+                                        current_object_id] = current_video_time  # Cập nhật thời gian bắt đầu detect
+                                    object_frequencies[current_object_id] = 1
+
+                            # Ghi lại các object có tần suất xuất hiện cao nhất trong 5 giây
+                            for obj_id, freq in object_frequencies.items():
+                                if freq > 2:
+                                    executed_objects.add(obj_id)
+
+                            # Khởi tạo tracker với bbox
+                            if not tracking or current_object_id != last_object_id:
+                                tracker.init(frame, current_bbox)
+                                tracking = True
+                                last_object_id = current_object_id
+                                break
+                    else:
+                        no_detection_count += 1
+
+                        if no_detection_count >= 2:
+                            tracking = False
+                            last_object_id = None
+
+
                 else:
-                    # Cập nhật tracker nếu vẫn theo dõi cùng một đối tượng
-                    success, bbox = tracker.update(frame)
-                    if success:
-                        (x, y, w, h) = [int(v) for v in bbox]
-                        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    no_detection_count += 1
 
-            else:
-                # Tăng biến đếm không phát hiện
-                no_detection_count += 1
+                    if no_detection_count >= 2:
+                        tracking = False
+                        last_object_id = None
 
-                # Nếu không có phát hiện trong 50 frame, dừng theo dõi
-                if no_detection_count >= 50:
-                    tracking = False
-                    last_object_id = None  # Reset ID đối tượng trước đó
         else:
-            # Tăng biến đếm không phát hiện
-            no_detection_count += 1
+            success, bbox = tracker.update(frame)
+            if success:
+                (x, y, w, h) = [int(v) for v in bbox]
+                x = max(0, x)
+                y = max(0, y)
+                w = min(w, frame.shape[1] - x)
+                h = min(h, frame.shape[0] - y)
 
-            # Nếu không có phát hiện trong 50 frame, dừng theo dõi
-            if no_detection_count >= 50:
-                tracking = False
-                last_object_id = None  # Reset ID đối tượng trước đó
-    else:
-        # Theo dõi đối tượng
-        success, bbox = tracker.update(frame)
-        if success:
-            # Vẽ bbox lên frame
-            (x, y, w, h) = [int(v) for v in bbox]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                object_color = object_color_map.get(last_object_id, (255, 255, 255))
 
-    cv2.imshow('Object Tracking', frame)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), object_color, 2)
+                label = f'Step : {last_object_id + 1}'
+                cv2.putText(frame, label, (x + 10, y - 10 if y - 10 > 10 else y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            object_color, 2)
 
-    # Thoát bằng phím 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow('Object Tracking', frame)
+        out.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+    time_step = convert_dict_to_min_sec(object_detection_start_times)
+    return time_step,output_path
+
+
+def convert_dict_to_min_sec(input_dict):
+    result = []
+    for index, value in input_dict.items():
+        seconds = value
+        minutes = int(seconds // 60)
+        seconds = int(seconds % 60)
+        result.append((index, f"{minutes}:{seconds:02d}"))
+    return result
+
+
+
+
+
+
